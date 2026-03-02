eba01940eb1712353bbdd7b5f86e3589a0a6f7be
GiampyCodes <giampysacco16@gmail.com>
2026-03-01 17:40:19 -0800

    Financial projects

---
+++ module_0.py
@@ -0,0 +1,6 @@
+import sys
+
+# This prints a message and shows exactly which Python is running
+print("Success! Your environment is active.")
+print(f"You are using Python version: {sys.version}")
+print(f"Running from: {sys.executable}")
